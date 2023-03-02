751b9f1ba25937f786a967c713956e903a9925a3
Elena Temyach <ea.lfornio@gmail.com>
2023-03-02 20:22:46 +0300

    profile

--- src/main/java/edu/school21/cinema/servlets/Profile.java
+++ src/main/java/edu/school21/cinema/servlets/Profile.java
@@ -14,9 +14,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
 
 @WebServlet("/profile")
 public class Profile extends HttpServlet {
@@ -34,6 +33,8 @@ public class Profile extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        HttpSession session = req.getSession();
+//        session.setAttribute("firstName", );
         ServletContext servletContext = req.getServletContext();
         RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(PROFILE);
         requestDispatcher.forward(req, resp);
--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -8,3 +8,13 @@ CREATE TABLE IF NOT EXISTS Users
     Password    VARCHAR(100) NOT NULL
     );
 
+CREATE TABLE IF NOT EXISTS Authorization
+(
+    Id          BIGSERIAL PRIMARY KEY,
+    Date        date NOT NULL,
+    Time        timestamp NOT NULL,
+    Ip          VARCHAR(30) NOT NULL,
+    UserId      BIGINT NOT NULL,
+    FOREIGN KEY (UserId) REFERENCES Users (Id)
+);
+
